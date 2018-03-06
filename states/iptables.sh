ab91c978dcd0322f0b68c11daaa586c23137a026
Olivier Mauras <olivier@mauras.ch>
2018-03-06 23:27:17 +0000

    Fix state

--- states/iptables.sh
+++ states/iptables.sh
@@ -2,12 +2,17 @@
 SHELL="/bin/sh"
 SRC="${TMPDIR}/files/etc/iptables/iptables.rules"
 DST="/etc/iptables/iptables.rules"
-SRCSUM=`sha1sum $SRC | cut -f 1 -d ' '`
-DSTSUM=`[ -f $DST ] && sha1sum $DST | cut -f 1 -d ' ' || echo "nofile"`
+SRCSUM=`[ -f $SRC ] && sha1sum $SRC | cut -f 1 -d ' ' || echo "nosrc"`
+DSTSUM=`[ -f $DST ] && sha1sum $DST | cut -f 1 -d ' ' || echo "nodst"`
 
-if [ "${SRCSUM}" == "${DSTSUM}" ]; then
+if [ "${SRCSUM}" = "${DSTSUM}" ]; then
   exit 2
 else
+  # Exit if we miss the source file
+  if [ "${SRCSUM}" = "nosrc" ]; then
+    echo "  !! Missing source file ${SRC}"
+    exit 1
+  fi
   cp -f ${SRC} ${DST}
   RET=$?
   if [ $RET -gt 0 ]; then
