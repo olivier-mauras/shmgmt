3883967a569edefa53cde295fd7d553850013969
Olivier Mauras <olivier@mauras.ch>
2018-03-12 18:36:40 +0000

    Major refactor of code. Separate modules and states. Support self installation

--- shmgmt.sh
+++ shmgmt.sh
@@ -1,65 +1,140 @@
 #!/bin/sh
-# This script tries to be a very stupid configuration management tool but a bit better than
-# a simple shell script
-# Requires: wget
 
-GITREPO="https://git.mauras.ch/Various/shmgmt"
+CONFIG="~/.shmgmt"
+INSTALLDIR="/usr/local/libexec/shmgmt"
+INSTALLBIN="/usr/local/bin/shmgmt"
+SHMGMTREPO="https://git.mauras.ch/shmgmt/shmgmt"
+STATEREPO="https://git.mauras.ch/shmgmt/states_example"
+STATEDIR="/tmp/shmgmt"
 GITCLONEURL="https://git.mauras.ch/Various/git-clone/raw/branch/master/git-clone"
-GITCLONE="/bin/git-clone"
+GITCLONEBIN="/usr/local/bin/git-clone"
 SHELL="/bin/sh"
-TMPDIR="/tmp/shmgmt"
 
-# Detect libc version - glibc or musl
-echo "==> Detect libc version"
-if `ldd --version 2>&1 | head -1 | grep musl >/dev/null 2>&1`; then
-  LIBC="musl"
-elif `ldd --version 2>&1 | head -1 | grep GNU >/dev/null 2>&1`; then
-  LIBC="glibc"
-else
-  echo "  ! Error unknown libc version"
-  exit 1
-fi
-
-# Check if git-clone is installed, if not install it
-if [ ! -f $GITCLONE ]; then
-  echo "==> Installing git-clone binary in PATH"
-  wget ${GITCLONEURL}_${LIBC} -O /bin/git-clone || exit 1
-  chmod 755 /bin/git-clone || exit 1
-fi
-
-# Git clone the repo
-if [ ! -d $TMPDIR ]; then
-  mkdir -p $TMPDIR
-else
-  rm -rf $TMPDIR
-  mkdir -p $TMPDIR
-fi
-
-echo "==> Cloning repo"
-$GITCLONE $GITREPO $TMPDIR > /dev/null 2>&1
-RET=$?
-if [ $RET -ne 0 ]; then
-  echo "  ! Error cloning repo"
-  exit 1
-fi
-
-# Load dist_check module
-. ${TMPDIR}/modules/dist_check
-
-# Get in there and loop over ./run/* scripts
-for SCRIPT in ${TMPDIR}/states/* ; do
-  BSCRIPT=`basename ${SCRIPT}`
-  echo "==> Applying ${BSCRIPT}"
-  TMPDIR=${TMPDIR} SHELL=${SHELL} DIST=${DIST} $SHELL ${SCRIPT}
-  RET=$?
-  if [ $RET -eq 0 ]; then
-    echo "==> ${BSCRIPT}: OK"
-  elif [ $RET -eq 1 ]; then
-    echo "==> ${BSCRIPT}: ERROR!"
-  else
-    echo "==> ${BSCRIPT}: Unknown return - $RET"
+_install_gitclone() {
+  # Check if git-clone is installed, if not install it
+  if [ ! -f $GITCLONE ]; then
+    echo "==> Installing git-clone binary in PATH"
+    [ ! -d `dirname $GITCLONEBIN` ] && mkdir -p `dirname $GITCLONEBIN` || exit 1
+    wget ${GITCLONEURL}_${LIBC} -O $GITCLONEBIN || exit 1
+    chmod 755 $GITCLONEBIN || exit 1
   fi
-done
+}
 
-# Cleanup local git clone
-rm -rf $TMPDIR
+_clone_repo() {
+  URL=$1
+  DESTDIR=$2
+
+  # Ensure that $DESTDIR is clean
+  if [ ! -d $DESTDIR ]; then
+    mkdir -p $DESTDIR
+  else
+    rm -rf $DESTDIR
+    mkdir -p $DESTDIR
+  fi
+
+  echo "==> Cloning $URL to $DESTDIR"
+  $GITCLONE $URL $DESTDIR > /dev/null 2>&1
+  RET=$?
+  if [ $RET -ne 0 ]; then
+    echo "  ! Error cloning repo"
+    exit 1
+  fi
+}
+
+_install() {
+  INSTALLDIR="$1"
+
+  # Detect libc version - glibc or musl
+  echo "==> Detect libc version"
+  if `ldd --version 2>&1 | head -1 | grep musl >/dev/null 2>&1`; then
+    LIBC="musl"
+  elif `ldd --version 2>&1 | head -1 | grep GNU >/dev/null 2>&1`; then
+    LIBC="glibc"
+  else
+    echo "  ! Error unknown libc version"
+    exit 1
+  fi
+
+  # Ensure git-clone binary is deployed
+  _install_gitclone
+
+  # Now install shmgmt locally
+  _clone_repo $SHMGMTREPO $INSTALLDIR
+
+  # Create shmgmt symlink
+  ln -sf ${INSTALLDIR}/shmgmt.sh $INSTALLBIN || exit 1
+
+  exit 0
+}
+
+_process_states() {
+  INSTALLDIR=$1
+  STATEDIR=$2
+
+  # Now install shmgmt locally
+  _clone_repo $STATEREPO $STATEDIR
+
+  # Load libs
+  for LIB in ${INSTALLDIR}/libs/*; do
+    . ${LIB}
+  done
+
+  # Loop over states
+  for STATE in ${STATEDIR}/states/* ; do
+    BSTATE=`basename ${STATE}`
+    echo "==> Applying ${BSTATE}"
+    INSTALLDIR=${INSTALLDIR} \
+      SHELL=${SHELL} \
+      DIST=${DIST} \
+      $SHELL ${STATE}
+    RET=$?
+    if [ $RET -eq 0 ]; then
+      echo "==> ${BSTATE}: OK"
+    elif [ $RET -eq 1 ]; then
+      echo "==> ${BSTATE}: ERROR!"
+    else
+      echo "==> ${BSTATE}: Unknown return - $RET"
+    fi
+  done
+}
+
+_cleanup() {
+  # Cleanup local git clone
+  rm -rf $STATEDIR
+}
+
+# Retrieve arguments
+while getopts ":c:r:di" opt; do
+  case $opt in
+    c)
+        CONFIG=$OPTARG
+        ;;
+    r)
+        STATEREPO=$OPTARG
+        ;;
+    d)
+        STATEDIR=$OPTARG
+        ;;
+    i)
+        INSTALLDIR=$OPTARG
+        _install $INSTALLDIR
+        ;;
+    *)
+        echo "  ! Unknown parameter \"$PARAM\""
+        exit 1
+        ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Load config if it exists
+if [ ! -z $CONFIG ] && [ -f $CONFIG ]; then
+  echo "==> Loading user config"
+  . $CONFIG
+fi
+
+# Sanity check
+if [ -z $STATEREPO ] || [ -z $STATEDIR ] || [ -z $INSTALLDIR ]; then
+  echo "!! Missing needed variables STATEREPO, STATEDIR or INSTALLDIR to work"
+fi
+
